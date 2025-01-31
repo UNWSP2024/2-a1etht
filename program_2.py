40a4c0e715d7ebc6d14bbe60dfa50192a0f2e685
Alethea Lo <aylo@students.unwsp.edu>
2025-01-30 21:52:39 -0600

    Add files via upload
    Title: Average Age 
    Programmer: Alethea Lo
    Date: 1/30/25

--- program_2.py
+++ program_2.py
@@ -1,13 +1,18 @@
+#Programmer: Alethea Lo
+#Date: 1/30/25
 
-def average_age():
-    # Get User Input
+#Program that calculates the average age of five friends
+age1=18
+age2=17
+age3=18
+age4=18
+age5=17
+
+#Calcuating the average age
+average_age = (age1 + age2 + age3 + age4 + age5)/5
+
+#Display the average age
+print("The average age of your five friends is:", average_age)
 
 
-    # Sum ages
 
-    # Average the ages
-
-    # Print the results
-
-# Line which calls the above function.
-average_age()
